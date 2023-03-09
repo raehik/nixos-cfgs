a1afb62a4d1129d40edbc9d518c54c046800d030
Ben Orchard <thefirstmuffinman@gmail.com>
2023-03-09 18:19:23 +0000

    cachix: +raehik

---
+++ modules/cachix/raehik.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://raehik.cachix.org"
+      ];
+      trusted-public-keys = [
+        "raehik.cachix.org-1:mtZ0Lj9YpxKxjrzdVsr6K8nM5uIJNCMgqTg4OGgypXI="
+      ];
+    };
+  };
+}
