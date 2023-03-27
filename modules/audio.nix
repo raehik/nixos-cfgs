68179e426fb3b03655d681829a8c9c341669a83b
Ben Orchard <thefirstmuffinman@gmail.com>
2023-03-27 15:33:28 +0100

    modules/audio: remove nonfunctioning config

--- modules/audio.nix
+++ modules/audio.nix
@@ -8,16 +8,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-
-    config.pipewire-pulse = {
-      "context.exec" = [
-        # among other stuff, gives nice "mute on unplug" behaviour
-        { path = "pactl";
-          args = "load-module module-switch-on-connect";
-        }
-      ];
-    };
-
   };
 
   environment.systemPackages = with pkgs; [
