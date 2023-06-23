b61251f14b95bddd5dba2536f31bee0ecb5309a0
Ben Orchard <thefirstmuffinman@gmail.com>
2023-06-23 14:41:37 +0100

    modules/cachix: +camfort

---
+++ modules/cachix/camfort.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://camfort.cachix.org"
+      ];
+      trusted-public-keys = [
+        "camfort.cachix.org-1:6mpQPMMUUCwkcTWyJ/5Up82cK+SIZ1bWsjLtJj+Boe4="
+      ];
+    };
+  };
+}
