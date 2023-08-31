b85e36391e866347ce0311f6132c4f014d7ed523
Ben Orchard <thefirstmuffinman@gmail.com>
2023-08-31 17:20:00 +0100

    modules/graphical: rework xdg portals

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -23,11 +23,7 @@
   xdg = {
     portal = {
       enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-	xdg-desktop-portal-gtk
-      ];
-      #gtkUsePortal = true; # 2022-12-13 raehik: deprecated
+      wlr.enable = true;
     };
   };
 
