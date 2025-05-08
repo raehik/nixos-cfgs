dba6efa5f29b33ce871c7f4b6f7eb22b6a3a5a32
Ben Orchard <thefirstmuffinman@gmail.com>
2025-05-08 22:41:48 +0100

    sw/graphical: -kochi-substitute (breaks things ._.)

--- modules/sw/graphical.nix
+++ modules/sw/graphical.nix
@@ -13,17 +13,48 @@
   # 2024-08-08: doesn't seem to work. lol. oh well idc just run manually
   services.xserver.desktopManager.runXdgAutostartIfNone = true;
 
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-    liberation_ttf
-    hack-font
-    fira-code
-    font-awesome # TODO 2022-12-14 specifically for i3status-rs...
-    roboto
-    kochi-substitute # MS Mincho replacement
-  ];
+  fonts = {
+
+    # 2025-05-08: uh, this seems to replace tons of my base CJK fonts...
+    # probably because I don't set defaults. which is complicated. urgh
+    /*
+    fontconfig.localConf = ''
+      <?xml version="1.0"?>
+      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+      <fontconfig>
+        <!-- fall back to Kochi Mincho for MS Mincho -->
+        <alias>
+          <family>ＭＳ 明朝</family>
+          <accept>
+            <family>Kochi Mincho</family>
+          </accept>
+        </alias>
+        <!-- fall back to Kochi Gothic for MS Gothic -->
+        <alias>
+          <family>ＭＳ ゴシック</family>
+          <accept>
+            <family>Kochi Gothic</family>
+          </accept>
+        </alias>
+      </fontconfig>
+    '';
+    */
+
+    };
+
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      liberation_ttf
+      hack-font
+      fira-code
+      font-awesome # TODO 2022-12-14 specifically for i3status-rs...
+      roboto
+      #kochi-substitute # MS Mincho replacement; TODO should be module sw/ja/font
+    ];
+
+  };
 
   # xdg desktop integration for screen sharing
   xdg = {
